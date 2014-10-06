d015416a9b29a5881615ea5ca24c12f215af7b8c
Alexander Minyushkin <alexander.minushkin@gmail.com>
2014-10-06 11:04:44 +0400

    Solution with sort implemented

--- majority-element.py
+++ majority-element.py
@@ -2,3 +2,11 @@ class Solution:
     # @param num, a list of integers
     # @return an integer
     def majorityElement(self, num):
+        num.sort()
+        return ( num[ len(num)/2 ])
+
+
+x = Solution()
+
+print x.majorityElement([1,2,3,9,8,7,2,2,2,2,2,2,2,2,2])
+
