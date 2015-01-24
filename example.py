5e1dd88fb1bb77cba94e198a2f1223d99149673f
Alexander Minyushkin <alexander.minushkin@gmail.com>
2015-01-24 12:35:15 +0300

    Class Solution is in use now

--- example.py
+++ example.py
@@ -1,27 +1,24 @@
-
-
-l = "([()])"
-
-
-closeDict = { '(':')' , '[':']' , '{':'}'}
-
-def isValid(s):
- #print s
- if len(s) == 0: return True
- start = s[0]
- if start in closeDict:
-   end = closeDict[start]
- else: return False
+class Solution:
+    # @return a boolean
+    def __init__(self):
+        self.closeDict = { '(':')' , '[':']' , '{':'}'}
+    
+    def isValid(self, s):
+        if len(s) == 0: return True
+        start = s[0]
+        if start in self.closeDict:
+            end = self.closeDict[start]
+        else: return False
  
- count = 0
- for i in range(0, len(s)):
-   if s[i] == start: count +=1
-   elif s[i] == end: count -=1
+        count = 0
+        for i in range(0, len(s)):
+            if s[i] == start: count +=1
+            elif s[i] == end: count -=1
   
-   if count == 0: break
+            if count == 0: break
 
- if count != 0: return False
- return ( isValid(s[1:i]) and isValid(s[i+1:]))
+        if count != 0: return False
+        return ( isValid(s[1:i]) and isValid(s[i+1:]))
 
 
 print isValid("({})[]") == True
